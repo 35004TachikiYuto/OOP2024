4489004967f62a3e8553ecf7304a12a0c4995040
TachikiYuto <oibc35004@ojs.ac.jp>
2024-04-24 15:57:05 +0900

    2.1.2

--- Chapter02/演習問題/Exercise1/Program.cs
+++ Chapter02/演習問題/Exercise1/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Exercise1 {
     internal class Program {
         static void Main(string[] args) {
+
+            var songs = new Song[] {
+                Song song
+
+
+
+            };
+            
+            
         }
+
+        //2.1.4
+        private static void PrintSongs(Song[] songs) {
+
+            Console.WriteLine(@"{0:hh\:mm\:ss}",TimeSpan.FromSeconds(153));
+        }
+
     }
 }
--- Chapter02/演習問題/Exercise2/Program.cs
+++ Chapter02/演習問題/Exercise2/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Exercise2 {
     internal class Program {
         static void Main(string[] args) {
+
+            
         }
     }
 }
