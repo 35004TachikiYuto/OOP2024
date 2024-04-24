ae8361d1dce4ba251f00025952360aea5317a519
TachikiYuto <oibc35004@ojs.ac.jp>
2024-04-24 15:17:00 +0900

    2.1-1完成

---
+++ Chapter02/演習問題/Exercise1/Song.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1 {
+    public class Song {
+
+        //歌のタイトル
+        public string Title { get; set; }
+        //アーティスト名
+        public string ArtistName { get; set; }
+        //演奏時間、単位は秒
+        public int Lengh { get; set; }
+    }
+}
