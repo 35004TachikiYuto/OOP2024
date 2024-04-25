9f3842ed18c299cf49659a3cec7bfb9985d0ca59
TachikiYuto <oibc35004@ojs.ac.jp>
2024-04-25 14:23:41 +0900

    演習問題終わり

--- Chapter02/演習問題/Exercise2/Program.cs
+++ Chapter02/演習問題/Exercise2/Program.cs
@@ -8,8 +8,28 @@ using System.Threading.Tasks;
 namespace Exercise2 {
     internal class Program {
         static void Main(string[] args) {
+            if (args.Length >= 1 && args[0] == "-tom") {
+                //インチからメートルへの対応表を出力
+                PrintInchToMeterList(int.Parse(args[1]), int.Parse(args[2]));
+            } else {
+                //メートルからインチの対応表を出力
+                PrintMeterToInchList(int.Parse(args[1]), int.Parse(args[2]));
+            }
 
-            
+        }
+
+        static void PrintMeterToInchList(int start, int stop) {
+            for (int meter = start; meter <= stop; meter++) {
+                double inch = InchConverter.FromMeter(meter);
+                Console.WriteLine("{0}m = {1:0.0000},in", meter, inch);
+            }
+        }
+
+        static void PrintInchToMeterList(int start, int stop) {
+            for (int inch = start; inch <= stop; inch++) {
+                double meter = InchConverter.ToMeter(inch);
+                Console.WriteLine("{0}in = {1:0.0000},m", inch, meter);
+            }
         }
     }
 }
---
+++ Chapter02/演習問題/Exercise2/YardConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2 {
+    public class YardConverter {
+        public static readonly double raito = 0.9144; //定数（外部にも公開する場合）
+
+        //メートルからインチを求める
+        public static double FromMeter(double meter) {
+            return meter / raito;
+        }
+        //インチからメートルを求める
+        public static double ToMeter(double yard) {
+            return yard * raito;
+        }
+    }
+}
