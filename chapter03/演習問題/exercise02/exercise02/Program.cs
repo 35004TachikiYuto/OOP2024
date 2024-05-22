112bbab4d9c68f65e5ca0979b31bc54e936224c5
TachikiYuto <oibc35004@ojs.ac.jp>
2024-05-22 10:29:42 +0900

    3.2.1完成

--- chapter03/演習問題/exercise02/exercise02/Program.cs
+++ chapter03/演習問題/exercise02/exercise02/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace exercise02 {
@@ -9,27 +10,51 @@ namespace exercise02 {
         static void Main(string[] args) {
             var names = new List<string> {
                  "Tokyo",
-                "New Delhi", 
-                "Bangkok", 
+                "New Delhi",
+                "Bangkok",
                 "London",
-                "Paris", 
-                "Berlin", 
-                "Canberra", 
+                "Paris",
+                "Berlin",
+                "Canberra",
                 "Hong Kong",
-};
+            };
+
+            Console.WriteLine("*****3.2.1*****");
             Exercise2_1(names);
             Console.WriteLine();
+
+            Console.WriteLine("*****3.2.1*****");
             Exercise2_2(names);
             Console.WriteLine();
+
+            Console.WriteLine("*****3.2.1*****");
             Exercise2_3(names);
             Console.WriteLine();
+
+            Console.WriteLine("*****3.2.1*****");
             Exercise2_4(names);
+            Console.WriteLine();
         }
 
+
         private static void Exercise2_1(List<string> names) {
+            Console.WriteLine("都市名を入力。空行で終了");
+
+            do {
+                var line = Console.ReadLine();
+                if (string.IsNullOrEmpty(line))
+                    break;//空行だった抜ける
+
+                int index = names.FindIndex(s => s == line);
+                Console.WriteLine(index + "番目");
+                Console.WriteLine("都市名を入力。空行で終了");
+            } while (true);
         }
 
         private static void Exercise2_2(List<string> names) {
+
+           
+
         }
 
         private static void Exercise2_3(List<string> names) {
