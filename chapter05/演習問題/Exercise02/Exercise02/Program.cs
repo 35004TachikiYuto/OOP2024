3d60c5afbbcbb4eb52744a6180fb97aedaf9c029
TachikiYuto <oibc35004@ojs.ac.jp>
2024-05-29 15:45:04 +0900

    問題5.1完成

---
+++ chapter05/演習問題/Exercise02/Exercise02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            var str = Console.ReadLine();
+            int num;
+            if(int.TryParse(str,out num ));
+        }
+    }
+}
