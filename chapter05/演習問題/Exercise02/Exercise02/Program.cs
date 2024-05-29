ab8d1f11ee14d68ffcede38dd63b372495f1a28a
TachikiYuto <oibc35004@ojs.ac.jp>
2024-05-29 15:49:52 +0900

    問題5.2完成

--- chapter05/演習問題/Exercise02/Exercise02/Program.cs
+++ chapter05/演習問題/Exercise02/Exercise02/Program.cs
@@ -7,9 +7,13 @@ using System.Threading.Tasks;
 namespace Exercise02 {
     internal class Program {
         static void Main(string[] args) {
+            Console.Write("数値を入力:");
             var str = Console.ReadLine();
             int num;
-            if(int.TryParse(str,out num ));
+            if(int.TryParse(str,out num)) {
+                string formattedNum = String.Format("{0:N0}", num);
+                Console.WriteLine(formattedNum);
+            }
         }
     }
 }
