739b94701912fa35eeb866b266218ff3a4afe349
TachikiYuto <oibc35004@ojs.ac.jp>
2024-06-18 11:14:24 +0900

    設問１_提出

--- TestProject/Test01/ScoreCounter.cs
+++ TestProject/Test01/ScoreCounter.cs
@@ -19,21 +19,25 @@ namespace Test01 {
                 var items = line.Split(',');
                 Student sale = new Student {
                     Name = items[0],
-                    ProductCategory = items[1],
-                    Amount = int.Parse(items[2]),
+                    Subject = items[1],
+                    Score = int.Parse(items[2]),
                 };
                 sales.Add(sale);
             }
             return sales;
-
-
-
-
         }
 
         //メソッドの概要： 
         public IDictionary<string, int> GetPerStudentScore() {
-
+            var dict = new Dictionary<string, int>();
+            foreach (var sale in _score) {
+                if (dict.ContainsKey(sale.Subject)) {
+                    dict[sale.Subject] += sale.Score;
+                } else {
+                    dict[sale.Subject] = sale.Score;
+                }
+            }
+            return dict;
 
 
 
