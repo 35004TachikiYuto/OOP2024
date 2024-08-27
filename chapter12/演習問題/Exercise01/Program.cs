f0a0f80dd3dc5713595bf0e2354eaca78cb6a411
TachikiYuto <oibc35004@ojs.ac.jp>
2024-08-27 12:06:59 +0900

    12.1.2完成

--- chapter12/演習問題/Exercise01/Program.cs
+++ chapter12/演習問題/Exercise01/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
@@ -16,14 +17,14 @@ namespace Exercise01 {
             Console.WriteLine(File.ReadAllText("employee.xml"));
             Console.WriteLine();
 
-           /*Exercise1_2("employees.xml");
+           Exercise1_2("employees.xml");
             Exercise1_3("employees.xml");
             Console.WriteLine();
 
             Exercise1_4("employees.json");
 
             // これは確認用
-            Console.WriteLine(File.ReadAllText("employees.json"));*/
+            Console.WriteLine(File.ReadAllText("employees.json"));
         }
 
         private static void Exercise1_1(string outfile) {
@@ -31,7 +32,7 @@ namespace Exercise01 {
             var employee = new Employee {
                 Id = 1,
                 Name = "橋本陸",
-                HireDate = new DateTime(2024-08-27)
+                HireDate = new DateTime(2024,08,27)
             };
 
             //シリアル化
@@ -50,9 +51,25 @@ namespace Exercise01 {
         }
 
         private static void Exercise1_2(string outfile) {
+            var emps = new Employee[] {
+                new Employee {
+                    Id=123,
+                    Name = "出井　秀行",
+                    HireDate=new DateTime(2001,5,10)
+                },
+                new Employee {
+                    Id = 139,
+                    Name = "大橋　孝仁",
+                    HireDate = new DateTime(2004,12,1)
+                },
+            };
+            using (var writer = XmlWriter.Create(outfile)) {
+                var serializer = new DataContractSerializer(emps.GetType());
+                serializer.WriteObject(writer, emps);
+            }
         }
-
         private static void Exercise1_3(string file) {
+            
         }
 
         private static void Exercise1_4(string file) {
