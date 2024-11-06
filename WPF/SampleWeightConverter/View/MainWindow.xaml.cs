351997757b8339efde3d6607d25ce8a9772d786b
TachikiYuto <oibc35004@ojs.ac.jp>
2024-11-06 15:58:23 +0900

    SampleWeightUnitConverter完成

--- WPF/SampleWeightConverter/View/MainWindow.xaml.cs
+++ WPF/SampleWeightConverter/View/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace SampleWeightConverter {
+namespace SampleWeightUnitConverter {
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
