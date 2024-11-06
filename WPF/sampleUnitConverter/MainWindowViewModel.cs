709238cb1d6e955e34cab5fa33331d6128027988
TachikiYuto <oibc35004@ojs.ac.jp>
2024-11-06 14:22:06 +0900

    SampleUnitConverter完成

---
+++ WPF/sampleUnitConverter/MainWindowViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SampleUnitConverter {
+    public class MainWindowViewModel : ViewModel {
+        private double metricValue, imperialValue;
+
+        //▲ボタンで呼ばれるコマンド
+        public ICommand ImperialUnitToMetric { get; private set; }
+        //▼ボタンで呼ばれるコマンド
+        public ICommand MetricToImperialUnit { get; private set; }
+
+        //上のComboBoxで選択されている値
+        public MetricUnit CurrentMetricUnit { get; set; }
+
+        //下のComboBoxで選択されている値
+        public ImperialUnit CurrentImprialUnit { get; set; }
+
+        public double MetricValue {
+            get { return metricValue; }
+            set {
+                metricValue = value;
+                OnPropertyChanged();    //値が変更されたら通知
+            }
+        }
+
+        public double ImperialValue {
+            get { return imperialValue; }
+            set {
+                imperialValue = value;
+                OnPropertyChanged();    //値が変更されたら通知
+            }
+        }
+
+        //コンストラクタ
+        public MainWindowViewModel() {
+            CurrentMetricUnit = MetricUnit.Units.First();
+            CurrentImprialUnit = ImperialUnit.Units.First();
+
+            MetricToImperialUnit = new DelegateCommand(() => 
+            ImperialValue = CurrentImprialUnit.FromMetricUnit(CurrentMetricUnit,MetricValue));
+
+            ImperialUnitToMetric = new DelegateCommand(() =>
+            MetricValue = CurrentMetricUnit.FromImperialUnit(CurrentImprialUnit, ImperialValue));
+        }
+    }
+}
