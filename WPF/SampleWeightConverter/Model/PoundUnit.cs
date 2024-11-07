f282e044773da63273195c6f53b31efd773da759
TachikiYuto <oibc35004@ojs.ac.jp>
2024-11-07 10:52:17 +0900

    トンとストーンを追加

--- WPF/SampleWeightConverter/Model/PoundUnit.cs
+++ WPF/SampleWeightConverter/Model/PoundUnit.cs
@@ -10,6 +10,8 @@ namespace SampleWeightUnitConverter {
         private static List<PoundUnit> units = new List<PoundUnit>() {
             new PoundUnit{ Name = "oz",Coefficient = 1,},
             new PoundUnit{ Name = "lb",Coefficient = 16,},
+            new PoundUnit{ Name = "stone",Coefficient = 224 ,},
+
         };
         public static ICollection<PoundUnit> Units { get { return units; } }
 
